de5aa2b4c1e36e46400763c9c904790cf93ef1ee
土豆电工 <tudoudiangong@163.com>
2019-12-10 07:23:06 +0800

    ..

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RSAKeyGen
+{
+    partial class KeyGen
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeyGen));
+            this.buttonRand = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // buttonRand
+            // 
+            this.buttonRand.Location = new System.Drawing.Point(12, 12);
+            this.buttonRand.Name = "buttonRand";
+            this.buttonRand.Size = new System.Drawing.Size(466, 208);
+            this.buttonRand.TabIndex = 2;
+            this.buttonRand.Text = "Rand";
+            this.buttonRand.UseVisualStyleBackColor = true;
+            this.buttonRand.Click += new System.EventHandler(this.buttonRand_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(416, 233);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(62, 18);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // KeyGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 260);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.buttonRand);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "KeyGen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KeyGen for RSA";
+            this.Load += new System.EventHandler(this.KeyGen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonRand;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSAKeyGen
+{
+    public partial class KeyGen : Form
+    {
+        public KeyGen()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyGen_Load(object sender, EventArgs e)
+        {
+            make();
+        }
+
+        private void buttonRand_Click(object sender, EventArgs e)
+        {
+            make(); 
+        }
+
+        private string GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+        private void make()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            String dir = GetTimeStamp();//使用时间戳做新的文件夹
+            buttonRand.Text = "Rand " + dir;
+            if (!Directory.Exists(dir))//若文件夹不存在则新建文件夹   
+            {
+                Directory.CreateDirectory(dir); //新建文件夹
+            }
+            using (StreamWriter streamWriter = new StreamWriter("./"+dir+"/PublicKey.xml"))
+            {
+                streamWriter.Write(rsa.ToXmlString(false));// 将公匙保存到运行目录下的PublicKey
+            }
+            using (StreamWriter streamWriter = new StreamWriter("./" + dir + "/PrivateKey.xml"))
+            {
+                streamWriter.Write(rsa.ToXmlString(true)); // 将公匙&私匙保存到运行目录下的PrivateKey
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/potatoDIY/keygen");
+        }
+    }
+}
